9907750bb75141a40c8f25b296aaa616d8f85c97
youssef4923 <99265413+youssef4923@users.noreply.github.com>
2024-09-03 12:08:40 +0300

    Delete pages directory

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-# pages/urls.py
-
-from django.urls import path
-from .views import hello_world
-
-urlpatterns = [
-    path('', hello_world, name='hello_world'),
-]
--- pages/views.py
+++
@@ -1,6 +0,0 @@
-# pages/views.py
-
-from django.http import HttpResponse
-
-def hello_world(request):
-    return HttpResponse("Hello world from Django!")
